4c165b1583556131c71b8965d5ee7043ddde42e9
Isaac Gordo Flores <isaacgflores.es@gmail.com>
2020-12-05 15:52:56 +0100

    Uso de useField y cambios varios

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
 import React from 'react';
-import { Formik, useFormikContext } from 'formik';
+import { Formik, useFormikContext, useField } from 'formik';
 import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 import * as Yup from 'yup';
+
+const MyInput = ({ fieldName, ...props }) => {
+
+    const [field] = useField(fieldName)
+
+    return ( <
+        TextInput style = {
+            styles.input
+        }
+        onChangeText = {
+            field.onChange('email')
+        }
+        value = {
+            field.value
+        }
+        { ...props }
+        />
+    )
+}
 
 const EmailForm = () => {
     const { handleChange, submitForm, values } = useFormikContext();
@@ -9,11 +28,8 @@ const EmailForm = () => {
     return (
         <>
         <Text>Correo electrónico</Text>
-        <TextInput
-            style= { styles.input }
-            onChangeText = { handleChange('email') }
-            value = { values.email }
-        />
+        <MyInput fieldName = "email" />
+        <MyInput fieldName = "name" />
         <Button onPress = { submitForm } title = "Enviar" />
         </>
     )
@@ -23,10 +39,15 @@ export default function App() {
 
     return (
         <View style = { styles.container }>
-            <Formik validationSchema = { Yup.object({
-                email: Yup.string()
-                    .email('Correo inválido')
-                    .required('Dato requerido')
+            <Formik
+                onSubmit = { x => console.log(x) }
+                validationSchema = { Yup.object({
+                    email: Yup.string()
+                        .email('Correo inválido')
+                        .required('Dato requerido'),
+                    name: Yup.string()
+                        .min(50)
+                        .required('Dato requerido')
             })}
             initialValues= {{ email: '' }}>
             <EmailForm />
@@ -46,6 +67,7 @@ const styles = StyleSheet.create({
       height: 30,
       width: 150,
       backgroundColor: '#eee',
+      border: '1px solid black',
     }
   });
   
